ce4c9b184d01a6d511a674f47a513bd591d5a157
Carl Boettiger <cboettig@gmail.com>
2026-02-01 21:49:00 -0800

    Add temporary download path for rclone script

--- inst/examples/download_cct_rclone.sh
+++ inst/examples/download_cct_rclone.sh
@@ -2,7 +2,7 @@
 # Optimized rclone script for downloading Caltech Camera Traps images from GCS
 # Saturates high-bandwidth networks for small file transfers
 
-rclone copy :gcs:public-datasets-lila/caltech-unzipped/cct_images ./cct_images \
+rclone copy :gcs:public-datasets-lila/caltech-unzipped/cct_images /tmp/cct_images \
     --gcs-anonymous \
     -P \
     --transfers 256 \
